685b92a838f2c6ecef9201190f86dc26aea2c8e8
Buket Soyhan <buketsoyhan1998@outlook.com>
2023-02-16 04:03:58 +0300

    Lasagna master functions

---
+++ Exercism-JS/lasagna-master.js
@@ -0,0 +1,37 @@
+/**
+ * Implement the functions needed to solve the exercise here.
+ * Do not forget to export them so they are available for the
+ * tests. Here an example of the syntax as reminder:
+ *
+ * export function yourFunction(...) {
+ *   ...
+ * }
+ */
+
+export function cookingStatus(timer) {
+  if (timer == 0) return "Lasagna is done.";
+  else if (timer > 0) return "Not done, please wait.";
+  else return "You forgot to set the timer.";
+}
+
+export function preparationTime(layers, timePerLayer = 2) {
+  return layers.length * timePerLayer;
+}
+
+export function quantities(ingredients) {
+  return {
+    noodles: ingredients.filter((i) => i === "noodles").length * 50,
+    sauce: ingredients.filter((i) => i === "sauce").length * 0.2,
+  };
+}
+
+export function addSecretIngredient(friendList, myList) {
+  myList.push(friendList[friendList.length - 1]);
+}
+export function scaleRecipe(recipe, amount) {
+  const scaled = {};
+  for (const key in recipe) {
+    scaled[key] = (recipe[key] * amount) / 2;
+  }
+  return scaled;
+}
